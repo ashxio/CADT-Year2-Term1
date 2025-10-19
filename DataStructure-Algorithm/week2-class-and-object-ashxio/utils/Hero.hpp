ec6392d89c6c7b500d1d91d53c55d79c99851656
ashxio <ashbookenhart@gmail.com>
2025-10-19 18:21:52 +0700

    yatta

--- DataStructure-Algorithm/week2-class-and-object-ashxio/utils/Hero.hpp
+++ DataStructure-Algorithm/week2-class-and-object-ashxio/utils/Hero.hpp
@@ -8,25 +8,64 @@ class Hero{
     private:
         // Define all the attributes here...
         // [ your code ]
-        
+        string role;
+        string name;
+        int health;
+        int armor;
+        int power;
+        string weapon;
         
     public:
         // Define the constructor and methods here ...
-        Hero(string name, int health, int armor, int power, string weapon){
+        Hero(string role, string name, int health, int armor, int power, string weapon){
             // assign each attributes' value from constructor param here...
             // [ your code ]
+            this->role = role;
+            this->name = name;
+            this->health = health;
+            this->armor = armor;
+            this->power = power;
+            this->weapon = weapon;
 
         }
     
         void info(){
             // process display hero info here...
             // [ your code ]
+            cout << "Role: " << role << endl;
+            cout << "Name: " << name << endl;
+            cout << "Health: " << health << endl;
+            cout << "Armor: " << armor << endl;
+            cout << "Power: " << power << endl;
+            cout << "Weapon: " << weapon << endl;
+
         }
 
         void strike(Hero& enemy){
             // process the strike logic here...
             // you can add the narative text as well (optional)
             // [ your code ]
+            cout << "ahh helly damnnn " << name << " tryna slime out " << enemy.name << " with a mythical " <<  weapon << "!" << endl;
+            int remainingPower = power;
+            if(enemy.armor > 0) {
+                if( remainingPower >= enemy.armor ) {
+                    remainingPower -= enemy.armor;
+                    enemy.armor = 0;
+                    cout << enemy.name << "'s armor is broken!" << endl;
+                    
+                } else {
+                    enemy.armor -= remainingPower;
+                    remainingPower = 0;
+                }
+            }
+            enemy.health -= remainingPower;
+            if(enemy.health < 0) {
+                enemy.health = 0;
+                cout << enemy.name << " has been defeated!" << endl;
+            }
+            else {
+                cout << enemy.name << " now has " << enemy.health << " HP and " << enemy.armor << " armor left." << endl;
+            }
 
         }
 
